511d5d9d7293192e5764b4064f37650b7588bd66
Branimir Maksimovic <branimir.maksimovic@gmail.com>
2021-06-27 01:41:51 +0200

    macos version added

---
+++ nbody/macoscompile.sh
@@ -0,0 +1,2 @@
+as nbody2.gas -o nbody2.o                                                                  
+ld -o nbody2 nbody2.o -lSystem -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
